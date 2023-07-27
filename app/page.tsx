558e1afbf9a578f99567d1d73ff746b4bd1610f9
VictorCiechovicz <victor09avila@hotmail.com>
2023-07-26 23:02:09 -0300

    feat:create form

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,143 @@
-import Image from 'next/image'
+'use client'
+import axios from 'axios'
+import { useState } from 'react'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+
+import { ChatCompletionRequestMessage } from 'openai'
+import { useToast } from '@/components/ui/use-toast'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+import { BrainCircuit, User } from 'lucide-react'
+
+const formSchema = z.object({
+  message: z.string().min(10, {
+    message: 'O mínimo de caracteres na pergunta é de 10 para ser enviada.'
+  })
+})
 export default function Home() {
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const { toast } = useToast()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: ''
+    }
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true)
+    const message: ChatCompletionRequestMessage = {
+      role: 'user',
+      content: values.message
+    }
+
+    const massagesInContext = [...messages, message]
+
+    try {
+      const response = await axios.post('/api/ai/conversation', {
+        messages: massagesInContext
+      })
+      setMessages(current => [...current, message, response.data])
+      toast({
+        variant: 'default',
+        title: '...Obaaa',
+        description: 'Mensagem enviada com sucesso'
+      })
+    } catch (error) {
+      console.log('OPEN_AI_ERROR:', error)
+      toast({
+        variant: 'destructive',
+        title: '...Opss',
+        description: 'Não foi possível enviar a mensagem.'
+      })
+    }
+
+    setIsLoading(false)
+    form.reset()
+  }
+
   return (
-    <>
-      pagina1
-      
-    </>
+    <div className="container mt-4 pt-4 h-full ">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ask for Advice</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isLoading}
+                    placeholder="Faça sua pergunta"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Inform the message.</FormDescription>
+                <Button
+                  type="submit"
+                  variant="secondary"
+                  className="w-full md:w-56"
+                  disabled={isLoading}
+                >
+                  Enviar
+                </Button>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      <div className="flex flex-col-reverse mt-4 space-y-2">
+        <div className="flex items-center justify-center">
+          {isLoading && (
+            <BrainCircuit className="container h-36 w-36 animate-pulse" />
+          )}
+        </div>
+        <div className="container">
+          {messages.map(menssage => (
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  {menssage.role === 'assistant' && (
+                    <BrainCircuit className="w-8 h-8" />
+                  )}
+                  {menssage.role === 'user' && <User className="w-8 h-8" />}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{menssage.content}</p>
+              </CardContent>
+              <CardFooter>
+                <p>{menssage.role}</p>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
