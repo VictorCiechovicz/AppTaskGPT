1dd5a20df10da28d654822b49e8a5173be530027
VictorCiechovicz <victor09avila@hotmail.com>
2023-07-27 21:47:18 -0300

    feat:create modal to chat

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-
+import Modal from 'react-modal'
 import {
   Form,
   FormControl,
@@ -29,7 +29,8 @@ import { useToast } from '@/components/ui/use-toast'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
-import { BrainCircuit, User } from 'lucide-react'
+import { BrainCircuit, User, MessagesSquare } from 'lucide-react'
+import Link from 'next/link'
 
 const formSchema = z.object({
   message: z.string().min(10, {
@@ -37,6 +38,7 @@ const formSchema = z.object({
   })
 })
 export default function Home() {
+  const [openChat, setOpenChat] = useState(false)
   const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([])
   const [isLoading, setIsLoading] = useState(false)
   const { toast } = useToast()
@@ -82,63 +84,80 @@ export default function Home() {
 
   return (
     <div className="container mt-4 pt-4 h-full ">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="message"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Ask for Advice</FormLabel>
-                <FormControl>
-                  <Input
-                    disabled={isLoading}
-                    placeholder="Faça sua pergunta"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-                <FormDescription>Inform the message.</FormDescription>
-                <Button
-                  type="submit"
-                  variant="secondary"
-                  className="w-full md:w-56"
-                  disabled={isLoading}
-                >
-                  Enviar
-                </Button>
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
-      <div className="flex flex-col mt-4 space-y-2">
-        <div className="flex items-center justify-center">
-          {isLoading && (
-            <BrainCircuit className="container h-36 w-36 animate-pulse" />
-          )}
+      <Modal
+        isOpen={openChat}
+        onRequestClose={() => setOpenChat(false)}
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <div className="container mt-4 pt-4 h-full ">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Ask for Advice</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="Faça sua pergunta"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription>Inform the message.</FormDescription>
+                    <Button
+                      type="submit"
+                      variant="secondary"
+                      className="w-full md:w-56"
+                      disabled={isLoading}
+                    >
+                      Enviar
+                    </Button>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+          <div className="flex flex-col mt-4 space-y-2">
+            <div className="flex items-center justify-center">
+              {isLoading && (
+                <BrainCircuit className="container h-36 w-36 animate-pulse" />
+              )}
+            </div>
+            <div className="container flex flex-col-reverse space-y-2">
+              {messages.map(menssage => (
+                <Card>
+                  <CardHeader>
+                    <CardTitle>
+                      {menssage.role === 'assistant' && (
+                        <BrainCircuit className="w-8 h-8" />
+                      )}
+                      {menssage.role === 'user' && <User className="w-8 h-8" />}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <p>{menssage.content}</p>
+                  </CardContent>
+                  <CardFooter>
+                    <p>{menssage.role}</p>
+                  </CardFooter>
+                </Card>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="container flex flex-col-reverse space-y-2">
-          {messages.map(menssage => (
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  {menssage.role === 'assistant' && (
-                    <BrainCircuit className="w-8 h-8" />
-                  )}
-                  {menssage.role === 'user' && <User className="w-8 h-8" />}
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>{menssage.content}</p>
-              </CardContent>
-              <CardFooter>
-                <p>{menssage.role}</p>
-              </CardFooter>
-            </Card>
-          ))}
+      </Modal>
+
+      <Link href="" onClick={() => setOpenChat(true)}>
+        <div className="flex justify-end">
+          <div className="bg-secondary hover:bg-secondary/80 w-10 h-10 rounded-full flex justify-center items-center">
+            <MessagesSquare />
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
